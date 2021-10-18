efd85825dfdabdd375e34d75d7e9648df9e8645b
tlaze <tomlazore@gmail.com>
2021-10-17 21:22:00 -0400

    Updated seeds.sql to match department_id and manager_id

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,28 @@
 INSERT INTO department (name)
-VALUES  ('Sales'),
-        ('HR'),
-        ('Accounting');
+VALUES  ('Engineering'),    
+        ('Finance'),        
+        ('Legal'),          
+        ('Sales');
 
 INSERT INTO roles (title, salary, department_id)
-VALUES  ('JR', 100000, 1),
-        ('Senior', 200000, 2),
-        ('Manager', 300000, 3);
+VALUES  ('Software Engineer', 100000, 1),
+        ('Lead Engineer', 200000, 1),
+        ('Accountant', 120000, 2),
+        ('Account Manager', 150000, 2),
+        ('Lawyer', 200000, 3),
+        ('Legal Team Lead', 300000, 3),
+        ('Sales Person', 100000, 4),
+        ('Sales Lead', 150000, 4);
+
+
+
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ('Tom', 'Lazore', 5, 2),
-        ('Tyler','M', 5, 3),
-        ('Bob', 'K', 6, 3);
+VALUES  ('Codey', 'McMullen', 1, NULL),
+        ('Ken','Leedy', 1, 1),
+        ('Kevin', 'Malone', 2, NULL),
+        ('Angela','Martin', 2, 2),
+        ('Michael','Ross', 3, NULL),
+        ('Harvey','Spector', 3, 3),
+        ('Stanley','Hudson', 4, NULL),
+        ('Jim','Halpert', 4, 4);   
