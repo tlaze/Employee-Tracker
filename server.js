d11f21ba2ec73381a9e2dcac1f03c553cb7916ec
tlaze <tomlazore@gmail.com>
2021-10-20 12:49:57 -0400

    Couldn't find a work around to not have previous input name be the same as the new manager name when updating employee role. Going to make a validation later that you can't choose a manager on the same id

--- server.js
+++ server.js
@@ -65,13 +65,14 @@ const viewAllEmployees = () =>{
                      roles.title AS Title,
                      roles.salary AS Salary,
                      department.name AS Department, 
-              CONCAT(employees.first_name, ' ' , employees.last_name) AS Manager
+              CONCAT(manager.first_name, ' ' , manager.last_name) AS Manager
               FROM employee 
-              LEFT JOIN employee employees ON employee.manager_id = employees.id
-              INNER JOIN roles ON employee.role_id = roles.id 
-              INNER JOIN department ON roles.department_id = department.id
+              
+              LEFT JOIN roles ON employee.role_id = roles.id 
+              LEFT JOIN department ON roles.department_id = department.id
+              LEFT JOIN employee manager ON manager.id = employee.manager_id
               ORDER BY employee.id;
-    `, (err,res) => {
+    `,(err,res) => {
         if(err){
             console.error(err);
         }
@@ -153,10 +154,8 @@ const chooseRole = () => {
 
 const chooseEmployee = () => {
     employeeList = [];
+    employeeList.push('This Employee is a Manager');
     db.query("SELECT * FROM employee", (err, name) => {
-        // console.log('--------');
-        // console.table(name);
-        // console.log('--------');
         if(err){
             console.error(err);
         }
@@ -164,7 +163,6 @@ const chooseEmployee = () => {
             for(i = 0; i < name.length; i++){
                 employeeList.push(`${name[i].first_name} ${name[i].last_name}`);
             }
-            employeeList.push('This Employee is a Manager');
         }
     });
     return employeeList;
@@ -175,73 +173,115 @@ const updateEmployeeRole = () => {
 ---------------------------------------------------------------------
                 UPDATING EMPLOYEE ROLE
 ---------------------------------------------------------------------`);
-    inquirer.prompt([
-        {
-            type: 'list',
-            message: "What is the ID Number You Want To Update?",
-            name: 'ID',
-            choices: [1,2,5]
-        },
-        {
-            type: 'input',
-            message: "What is the New Employee's First Name?",
-            name: 'firstName'
-        },
-        {
-            type: 'input',
-            message: "What is the New Employee's Last Name?",
-            name: 'lastName'
-        },
-        {
-            type: 'list',
-            message: "What is the Employee's New Role?",
-            name: 'role',
-            choices: chooseRole()
-        },
-        {
-            type: 'list',
-            message: "Who is the Employee's manager?",
-            name: 'manager',
-            choices: chooseEmployee()
-        },
-    ])
-    .then((answer) => {
-        let roleID = roleList.indexOf(answer.role) + 1;
-        let managerID = employeeList.indexOf(answer.manager)+1;
-        console.log("ManagerID: " + managerID);
-        console.log(employeeList);
-        console.log(employeeList.indexOf(answer.manager));
-        console.log(typeof managerID);
 
-        // if(managerID === employeeList[0].id)
-
-        if(answer.manager === 'This Employee is a Manager'){
-            managerID = null;
+    
+    db.query(`  SELECT employee.id, employee.first_name AS FirstName, employee.last_name AS LastName FROM employee ORDER BY employee.id;`,(err,res) => {
+        if(err){
+            console.error(err);
         }
-        // else if(`${answer.firstName} ${answer.lastName} === ${answer.manager}`){
-        //     managerID = null;
-        // }
-        db.query(`
-            UPDATE employee
-            SET first_name = "${answer.firstName}",
-                last_name = "${answer.lastName}",
-                role_id = "${roleID}",
-                manager_id = ${managerID}
-            WHERE id = "${answer.ID}"`, (err,res) => {
-                if(err){
-                    console.error(err);
-                }
-                else{
-                    console.log(`
-            -------------------------------------------------------------------------------
-                        ${answer.firstName} ${answer.lastName} Is Now Updated In The Employee Database!
-            -------------------------------------------------------------------------------`);
-                    initializeQuestions();
+        else{
+            console.table(res);
+        }
+    });
 
-                }
-            }); 
+
+
+
+    db.query('SELECT * FROM employee', (err, res) => {
+        if(err){
+            console.error(err);
+        }
+        else{
+
+            const idArray = res.map(function(i) {
+                return `${i.id}`;
+            })
+
+            const managerArray = res.map(function(manager) {
+                return `${manager.first_name} ${manager.last_name}`;
+            });
+
+            //Deletes the first index value. When choosing an updated manager the first choice is the previous employee input.
+            //Because the ID matched the managerID, the new employee entered would also display as manager instead of the name chosen.
+            //Since this deletes the first value, there is no issue anymore.
+            managerArray.push('This Employee is a Manager');
+            console.log(managerArray);
+        
+
+        
             
-    })
+            inquirer.prompt([
+                {
+                    type: 'list',
+                    message: "What is the ID Number You Want To Update?",
+                    name: 'ID',
+                    choices: idArray
+                },
+                {
+                    type: 'input',
+                    message: "What is the New Employee's First Name?",
+                    name: 'firstName'
+                },
+                {
+                    type: 'input',
+                    message: "What is the New Employee's Last Name?",
+                    name: 'lastName'
+                },
+                {
+                    type: 'list',
+                    message: "What is the Employee's New Role?",
+                    name: 'role',
+                    choices: chooseRole()
+                },
+                {
+                    type: 'list',
+                    message: "Who is the Employee's manager?",
+                    name: 'manager',
+                    choices: managerArray
+                },
+            ])
+            .then((answer) => {
+                console.log("answer ", answer);
+        
+                // // employeeList.filter(answer.manager => employees.id != managerID);
+                let roleID = roleList.indexOf(answer.role) + 1;
+                let managerID = managerArray.indexOf(answer.manager) + 1;
+                console.log(managerID);
+                console.log(answer.manager);
+        
+                if(answer.manager === 'This Employee is a Manager'){
+                    managerID = null;
+                }
+
+                db.query(`
+                UPDATE employee
+                SET first_name = "${answer.firstName}",
+                    last_name = "${answer.lastName}",
+                    role_id = "${roleID}",
+                    manager_id = ${managerID}
+                WHERE id = "${answer.ID}"`, (err,res) => {
+                    if(err){
+                        console.error(err);
+                    }
+                    else{
+                        console.log(`
+                -------------------------------------------------------------------------------
+                            ${answer.firstName} ${answer.lastName} Is Now Updated In The Employee Database!
+                -------------------------------------------------------------------------------
+                `);
+                        console.table(res.employee);
+                        initializeQuestions();
+    
+                    }
+                }); 
+
+
+                
+            })
+            
+        }
+    
+    });
 }
 
 
